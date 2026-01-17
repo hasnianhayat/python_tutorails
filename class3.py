77ab7b35845d50f6bd1418ea84f998b4aa8b5cce
hasnianhayat <your-email-used-on-github>
2026-01-17 19:47:44 +0500

    change done

--- class3.py
+++ class3.py
@@ -1,11 +1,31 @@
 # Conditional statment
 
+"""
 age = 13
 if(age >= 18):
     print("you can drive")
+"""
+
+
+Marks = int(input("Enter your marks: "))
+
+if(Marks >= 90):
+    print("A+ Grade")
+
+elif(Marks >= 80):
+    print("A Grade")
+
+elif(Marks >= 70):
+    print("B Grade")
+
+elif(Marks >= 60):
+    print("C Grade")
+
+else:
+    print("Fail")
+
+
     
 
-    # kjadlka
-else:
-    print("you can not drive")
+
 
