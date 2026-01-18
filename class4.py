5a3f8940d82f1cba6840ab396778e70ee52c3176
hasnianhayat <your-email-used-on-github>
2026-01-18 15:28:58 +0500

    change done

---
+++ class4.py
@@ -0,0 +1,22 @@
+# list in python
+
+# list = [1, 2, 3, 4, 5]
+
+
+# print(type(list))  
+# print(len(list))
+# print("Original List:", list)
+# print("First Element:",list[0])  # accessing first element
+# print("Last Element:",list[-1])   # accessing last element
+# print(list[0:3])  # slicing the list from index 0 to 2
+# print("List Repeated:", list * 2)  # repeating the list
+
+
+
+list1 = ["apple", "banana", "cherry"]
+list2 = ["orange", "kiwi", "melon"]
+
+newlist = list1 + list2
+print("Concatenated List:", newlist)
+
+
--- prictice.js
+++ prictice.js
@@ -1,11 +1,23 @@
 
-let age = 20;
+// let age = 20;
 
-if(age >= 18 ){
-    console.log("you can drive")
-}else{
-    console.log("you can not dirve")
-}
+// if(age >= 18 ){
+//     console.log("you can drive")
+// }else{
+//     console.log("you can not dirve")
+// }
+
+
+const arry = [1,2,3,4,5];
+const arry2 = [6,7,8,9,10];
+
+// const combinedArray = [...arry, ...arry2];
+
+const combinedArray = 
+
+
+console.log(combinedArray);
+
 
 
 
