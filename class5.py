85275cc3a31a9bd48de2de55dd16ad3fe19c2846
hasnianhayat <your-email-used-on-github>
2026-01-22 18:44:00 +0500

    change done

--- class5.py
+++ class5.py
@@ -1,13 +1,29 @@
 # Python Tuple
 
-# tuple = (2,"hello",4.5)
+tuple = (2,"hello",4.5)
 # print(type(tuple))
 # print(tuple)
 # print(tuple[1])
+# print(tuple.index("hello"))
 
-tup = (1,2,3,4,5,6,7,8,9,10)
+tup = (1,2,3,4,5,6,7,8,2,2,2,9,10)
 
-print(tup[:10])
+# print(tup[0:5])
+# print(tup[0:])
+# print(tup[:3])
+# print(tup[-5:-1])
+# print(tup.count(2))
+# print(len(tup))
+
+# tup1 = (11,12,13) 
+# tup2 = (14,15,16)
+# tup3 = tup1 + tup2
+# print(tup1)   
+
+tup1 = (11,12,13) * 3
+
+
+
 
 
 
