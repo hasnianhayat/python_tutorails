d283dda17ddae4c76d7b24cb43fde14db5812325
hasnianhayat <your-email-used-on-github>
2026-01-22 19:03:28 +0500

    python dict

--- class6.py
+++ class6.py
@@ -11,6 +11,9 @@
 dic = dict(
     name = "hasnain",
     age = 21,
+    isStudent = True,
+    ismarried = False
+
 )
 
 # print(type(dic))
@@ -21,3 +24,7 @@ dic = dict(
 # dic["age"] = 22
 # dic["city"] = "Peshawar"
 # print(dic)
+
+# del dic["ismarried"]
+# print(dic.pop("isStudent"))
+print(dic.clear())
