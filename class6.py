be1f300a86b11e3682142de1d7bc0a2d4538ada1
hasnianhayat <your-email-used-on-github>
2026-01-22 18:58:38 +0500

    change done

--- class6.py
+++ class6.py
@@ -1,2 +1,23 @@
 # Python  Dictionary
 
+# dict = {
+#     "name":"hasnain",
+#     "age":21,
+# }
+
+# print(type(dict))
+# print(dict)
+
+dic = dict(
+    name = "hasnain",
+    age = 21,
+)
+
+# print(type(dic))
+# print(dic)
+
+# print(dic["name"])
+# print(dic.get("age"))  # Output: 21
+# dic["age"] = 22
+# dic["city"] = "Peshawar"
+# print(dic)
