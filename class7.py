691c27d7046406c7b276fae6a6864c3e6630db78
hasnianhayat <your-email-used-on-github>
2026-01-26 12:04:56 +0500

    change done

---
+++ class7.py
@@ -0,0 +1,43 @@
+# Python Loops
+
+# Loop in Python are used to iterate over a block of code multiple times until a certain condition is met.
+
+# loop are use to  repeat  a block of code again and again 
+
+# count = 1
+
+# while count <= 100:
+#     print(count)
+#     count += 1  # count = count + 1
+
+# print("Loop Ended")
+# print(count)
+
+
+# count = 100
+
+# while count >= 1:
+#     print(count)
+#     count -= 1  # count = count + 1
+
+# print("Loop Ended")
+# print(count)
+
+
+# n = 1
+# table_of = 6
+
+# while n <= 10:
+#         print(table_of,"x ",n," = ",table_of*n)
+#         n += 1
+
+
+list = [1,2,3,4,5,6,7,8,9,10]
+
+# while len(list) > 0:
+#     print(list)
+#     list.pop()  # removes the last element from the list
+
+
+# while len(list) :
+#     print(list)
